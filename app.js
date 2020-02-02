85ddd6642d820027cc8787c1cf3615578d41b5c4
tparadizzo <paradizzotabita@gmail.com>
2020-02-02 18:28:44 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,39 @@
+// Question 3
+var firstName = 'Tabita' ;
+
+// Question 4
+var favouriteColor = 'Pink';
+
+// Question 5
+var favouriteFood = 'Gyoza';
+var city = 'Dublin';
+
+/* Question 6
+Answer: x have Toe as value.
+*/
+
+/* Question 7
+Answer: y have Laurel as value
+*/
+
+// Question 8 and 9
+// Prints Tabita
+console.log(firstName);
+// Prints Pink
+console.log(favouriteColor);
+// Prints Gyoza and Dublin in he same line
+console.log(favouriteFood, city); 
+
+// Question 10
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+// Prints all phrase
+console.log(phrase + ' ' + one + ', ' + two + ', ' + three + ', ' + four + ', ' + five + ', ' + six + ', ' + seven + ' ' + endingPhrase);
